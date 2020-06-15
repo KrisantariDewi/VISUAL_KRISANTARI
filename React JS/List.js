8c144fd2a0a34e6d9dd8e0c37c8796ca97f219ca
KrisantariDewi <61818923+KrisantariDewi@users.noreply.github.com>
2020-06-14 23:32:11 -0800

    Video React Part 1 dan Part 2

---
+++ React JS/List.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+
+class List extends Component {
+    render() {
+        return (
+        <div>
+            <h3> Ini List</h3>
+        </div>
+        )
+    }
+}
+
+export default List
---
+++ React JS/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+
+class Sidebar extends Component {
+    render() {
+        return (
+        <div>
+            <h3> Ini Sidebar</h3>
+        </div>
+        )
+    }
+}
+
+export default Sidebar
